89e6cb38a270b4fe2ef2d74068e82e5e61f11831
Henrik Vasquez <123214691+H-Vasq@users.noreply.github.com>
2024-09-21 18:39:37 +0000

    test and run

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
+      {/* { <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
           GitHub Codespaces <span className="heart">♥️</span> React
@@ -20,8 +20,22 @@ function App() {
           >
             Learn React
           </a>
-        </p> */}
-      </header>
+        </p>} */}
+        <section className="card-container">
+          <header>
+            <span initials="JS"></span>
+            <h2>Joon Smith</h2>
+          </header>
+
+          <main>
+            <ul>
+              <li><span>Birthday</span> Jan 1, 1990</li>
+              <li><span>Address</span> 123 Fulton St.</li>
+              <li><span>Phone</span> 555-555-5555</li>
+            </ul>
+          </main>
+        </section>
+      {/* </header> */}
     </div>
   );
 }
